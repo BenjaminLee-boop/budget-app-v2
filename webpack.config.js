b1619a7de309718c4494f4a953a5159a1bfa909f
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-04-28 15:23:50 +0930

    Does This work
    Does This work

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "production",
+  entry: {
+    app: "./public/index.js",
+    db: "./public/db.js",
+    serviceWorker: "./public/service-worker.js"
+  },
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget App",
+      short_name: "Budget",
+      description: "A PWA for budgeting",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("public/icons/icon-192x192.png"),
+        sizes: [192, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
